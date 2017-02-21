d7c6528cc238e9eff29efb7e179d036000b8a843
Jacob Lutin <jacoblutin@MacBook-Pro-Jacob.local>
2017-02-21 19:48:54 +0300

    Vectors math implemented in additon lib. Vector2 class added

---
+++ vectormath.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <cmath>
+
+Vector3::Vector3(float x, float y, float z)
+{
+	this->x = x;
+	this->y = y;
+	this->z = z;
+}
+
+float Vector3::LengthSquared()
+{
+	return x * x + y * y + z * z;
+}
+
+float Vector3::Length()
+{
+	return sqrt(LengthSquared());
+}
+
+void Vector3::Print()
+{
+	std::cout << "Vector3 = " << x << " " << y << " " << z << std::endl;
+	return;
+}
+
+Vector3 Vector3::operator+(Vector3 const &v)
+{
+	return Vector3(x + v.x, y + v.y, z + v.z);
+}
+
+Vector3 Vector3::operator-(Vector3 const &v)
+{
+	return Vector3(x - v.x, y - v.y, z - v.z);
+}
+
+float Vector3::operator*(Vector3 const &v)
+{
+	return x * v.x + y * v.y + z * v.z;
+}
+
+bool Vector3::operator==(Vector3 const &v)
+{
+	return x == v.x && y == v.y && z == v.z;
+}
+
+void Vector3::operator*=(const float n)
+{
+	x = x * n;
+	y = y * n;
+	z = z * n;
+	return;
+}
+
+
+Vector2::Vector2(float x, float y)
+{
+	this->x = x;
+	this->y = y;
+}
+
+float Vector2::LengthSquared()
+{
+	return x * x + y * y;
+}
+
+float Vector2::Length()
+{
+	return sqrt(LengthSquared());
+}
+
+void Vector2::Print()
+{
+	std::cout << "Vector3 = " << x << " " << y << std::endl;
+	return;
+}
+
+Vector2 Vector2::operator+(Vector2 const &v)
+{
+	return Vector3(x + v.x, y + v.y);
+}
+
+Vector2 Vector2::operator-(Vector2 const &v)
+{
+	return Vector3(x - v.x, y - v.y);
+}
+
+float Vector2::operator*(Vector2 const &v)
+{
+	return x * v.x + y * v.y + z * v.z;
+}
+
+bool Vector2::operator==(Vector2 const &v)
+{
+	return x == v.x && y == v.y;
+}
+
+void Vector2::operator*=(const float n)
+{
+	x = x * n;
+	y = y * n;
+	return;
+}
---
+++ vectormath.h
@@ -0,0 +1,44 @@
+#ifndef VECTORMATH_H
+#define VECTORMATH_H
+
+#include <iostream>
+#include <cmath>
+
+class Vector3
+{
+public:
+	float x;
+	float y;
+	float z;
+	Vector3() { x = y = z = 0; };
+	Vector3(float x, float y, float z);
+	Vector3 operator+(Vector3 const &v);
+	Vector3 operator-(Vector3 const &v);
+	bool operator==(Vector3 const &v);
+	float operator*(Vector3 const &v);
+	void operator*=(const float n);
+	float LengthSquared();
+	float Length();
+	void Print();
+	// ~Vector3();
+};
+
+class Vector2
+{
+public:
+	float x;
+	float y;
+	Vector2() { x = y = 0; };
+	Vector2(float x, float y);
+	Vector3 operator+(Vector2 const &v);
+	Vector3 operator-(Vector2 const &v);
+	bool operator==(Vector2 const &v);
+	float operator*(Vector2 const &v);
+	void operator*=(const float n);
+	float LengthSquared();
+	float Length();
+	void Print();
+	// ~Vector2();
+};
+
+#endif
