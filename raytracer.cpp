330f36fdc1ef8908947eeb90f9c9fd75a7f6a767
Jacob Lutin <jacoblutin@MacBook-Pro-Jacob.local>
2017-02-21 19:33:20 +0300

    Color class implemented

--- raytracer.cpp
+++ raytracer.cpp
@@ -73,6 +73,22 @@ void Vector::operator*=(const float n)
 	return;
 }
 
+class Color
+{
+public:
+	double r, g, b;
+	Color() {r = 0; g = 0; b = 0};
+	Color(double r, double g, double b);
+};
+
+Color::Color(double r, double g, double b)
+{
+	this->r = r;
+	this->g = g;
+	this->b = b;
+	return;
+}
+
 int main()
 {
 	Vector v(1, 2, 3);
