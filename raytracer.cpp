c86c14431da90389f689b83ce41ea0cbb06513f4
Jacob Lutin <jacoblutin@MacBook-Pro-Jacob.local>
2017-02-21 16:07:14 +0300

    Initial commit. Some Vector function implemented

---
+++ raytracer.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <cmath>
+
+
+class Vector
+{
+public:
+	float x;
+	float y;
+	float z;
+	Vector() { x = y = z = 0; };
+	Vector(float x, float y, float z);
+	Vector operator+(Vector const &v);
+	Vector operator-(Vector const &v);
+	bool operator==(Vector const &v);
+	float operator*(Vector const &v);
+	void operator*=(const float n);
+	float LengthSquared();
+	float Length();
+	void Print();
+	// ~Vector();
+};
+
+Vector::Vector(float x, float y, float z)
+{
+	std::cout << "vector instance\n";
+	this->x = x;
+	this->y = y;
+	this->z = z;
+}
+
+float Vector::LengthSquared()
+{
+	return x * x + y * y + z * z;
+}
+
+float Vector::Length()
+{
+	return sqrt(LengthSquared());
+}
+
+void Vector::Print()
+{
+	std::cout << "vector = " << x << " " << y << " " << z << std::endl;
+	return;
+}
+
+Vector Vector::operator+(Vector const &v)
+{
+	return Vector(x + v.x, y + v.y, z + v.z);
+}
+
+Vector Vector::operator-(Vector const &v)
+{
+	return Vector(x - v.x, y - v.y, z - v.z);
+}
+
+float Vector::operator*(Vector const &v)
+{
+	return x * v.x + y * v.y + z * v.z;
+}
+
+bool Vector::operator==(Vector const &v)
+{
+	return x == v.x && y == v.y && z == v.z;
+}
+
+void Vector::operator*=(const float n)
+{
+	x = x * n;
+	y = y * n;
+	z = z * n;
+	return;
+}
+
+int main()
+{
+	Vector v(1, 2, 3);
+	Vector u(1, 1, 1);
+	Vector r;
+	r = v + u;
+	r *= 2;
+	r.Print();
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
